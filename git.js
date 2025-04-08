c8c65f96db30b84f0a8c87c4ca2c49501f240f34
João Pedro <Joo.p.esteves@aluno.senai.br>
2025-04-08 10:56:55 -0300

    branch

--- git.js
+++ git.js
@@ -2,3 +2,4 @@ let comidas = ["Lasanha", "Pizza", "Sushi", "Hamburguer", "Batata frita"]
 console.log("Todas as comidas", comidas);
 comidas.sort()
 console.log(`comidas cordenadas ${comidas}`);
+console.log('teste')
